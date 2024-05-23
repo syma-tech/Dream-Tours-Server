7192757964b324bc2e878de3be506cdc2dae4aec
“syma-tech” <symarahman17@gmail.com>
2024-05-23 12:11:01 +0600

    update 1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -30,13 +30,21 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    client.connect();
+    // await client.connect();
 
     const toursCollection = client.db("dreamTourDB").collection("tours");
 
     app.get("/tours", async (req, res) => {
       const cursor = toursCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/tours/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toursCollection.findOne(query);
       res.send(result);
     });
 
